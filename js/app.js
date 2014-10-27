72f64adf69932d46bc97cf4d71cfab381be2cf13
Gwan Yip <gwan.yip26@gmail.com>
2014-10-27 17:37:12 -0400

    Added setting icon drop down on click

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Setting icon drop down on click
+
+var settingIcon = $(".settings-icon");
+var settingIconDropdown = $("#settings-icon-dropdown");
+
+settingIcon.click(function(){
+   settingIconDropdown.toggle();
+})
